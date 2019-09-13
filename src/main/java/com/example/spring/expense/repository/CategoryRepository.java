10c2e2d0e50945be8a237bd9acb9390a4d058ed0
TylerCEdge <tylercedge@gmail.com>
2019-09-12 23:13:52 -0400

    mapped database to Jpa and seeded database (with some difficulties.  STS4 apparently has some issues with .sql files currently.  Used VS Code to write that portion.  Will test to verify it works soon.

---
+++ src/main/java/com/example/spring/expense/repository/CategoryRepository.java
@@ -0,0 +1,5 @@
+package com.example.spring.expense.repository;
+
+public interface CategoryRepository {
+
+}
---
+++ src/main/resources/Data.sql
@@ -0,0 +1,14 @@
+
+insert into user values (1,'Tyler','tylercedge@gmail.com')
+insert into user values (2,'Steve','steve@steve.com')
+insert into user values (3,'Chris','chris@chris.com')
+
+
+insert into category values (1,'Travel')
+insert into category values (2,'Auto Loan')
+insert into category values (3,'Travel')
+
+
+insert into expense values (100,'New York','2019-09-11T17:00:00.000Z',1,1)
+insert into expense values (101,'Ford Mustang','2019-09-06T13:00:00.000Z',2,2)
+insert into expense values (102,'Grand Canyon','2019-09-01T11:00:00.000Z',3,1)
